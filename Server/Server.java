caac5f3e160eaf5e8737210e570a8a3d44cf4f9c
leandro08 <leandrollano82@gmail.com>
2017-04-30 21:07:21 +0200

    Multi-Client Server

--- Server/Server.java
+++ Server/Server.java
@@ -1,31 +1,76 @@
+import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
-public class Server{
+public class Server extends Thread{
+	
+	private BufferedReader in_cmd;
+	private PrintWriter out;
+	private int ID;
 
-    public static void main(String [] args){
+	public Server(BufferedReader inputChannel, PrintWriter outputChannel, int ID){
+		this.in_cmd= inputChannel;
+		this.out= outputChannel;
+		this.ID= ID;
+	}
 
-        System.out.println(" Waiting for the connection...\n");
+	public void run(){
+		try{
+			int index= 0;	
+			String read= "";
 
-        try{
-            ServerSocket server= new ServerSocket(4545);
-            Socket sockets= null;
-            Scanner messages= new Scanner(System.in);
-            BufferedReader nickname;
-            SetSockets thread= new SetSockets(10);
-            int index= 0;
+			while(true){
+				if(in_cmd.ready()){
+					read= in_cmd.readLine();
 
-            while(index < 11){
-                sockets= server.accept();
-                nickname= new BufferedReader(new InputStreamReader(sockets.getInputStream()));
-                System.out.println("New connection from "+sockets.getInetAddress());
-                thread.setSockets(sockets, nickname.readLine(), index);
-                thread.runing(index);
-                index++;
-            }
-        }
-        catch (Exception e){
-            System.out.println("Problems with the connection!");
-        }
-    }
+					if(read.length()>5){
+						index= Integer.parseInt(read.split("-")[1]);
+						read= read.split("-")[0];}
+
+					if(read.equals(":LIST")){
+						Commands.getUsers(out);}
+
+					else if(read.equals(":JOIN")){
+						Commands.setChat(index, in_cmd, out, ID);}
+		     	}
+			Thread.sleep(15);
+		   }
+		}
+		catch(Exception ex){System.out.println("Problems with the IO channels...");}
+	}
+
+	public static void main(String [] args){
+		
+		System.out.println("Waiting for the connection...\n");
+
+		try{
+			BufferedReader input= null;
+			PrintWriter output= null;
+			ServerSocket server= new ServerSocket(4545);
+		
+			Socket socket= null;
+			String nickname= "";
+			Server thread= null;
+
+			int index= 0;
+
+			while(true){
+				socket= server.accept();
+				output= new PrintWriter(socket.getOutputStream(), true);
+				output.println(index);
+				output.println("Enter a nickname to login:");
+
+				input= new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				nickname= input.readLine();
+				DataClient.set(socket, nickname);
+
+				System.out.println("New connection from "+socket.getInetAddress());
+				thread= new Server(input, output, index);
+				thread.start();
+				index++;
+	
+			}
+		}
+		catch(Exception ex){System.out.println("Problems with the connection");}
+	}
 }
