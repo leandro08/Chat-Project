eb75814ba3d5441cc3aeda1df23d3414c16934b1
leandro08 <leandrollano82@gmail.com>
2017-04-30 21:12:38 +0200

    Data of the clients

--- Server/Data Client/DataClient.java
+++ Server/Data Client/DataClient.java
@@ -1,1 +1,21 @@
+import java.net.Socket;
+import java.util.ArrayList;
 
+public class DataClient{
+
+	public static ArrayList<Socket> sockets= new ArrayList<Socket>();
+	public static ArrayList<String> nicknames= new ArrayList<String>();
+
+	public static void set(Socket socket, String nickname){
+		sockets.add(socket);
+		nicknames.add(nickname);
+	}
+	
+	public static Socket getSocket(int index){
+		return sockets.get(index);
+	}
+	
+	public static String getNickname(int index){
+		return nicknames.get(index);
+	}
+}
